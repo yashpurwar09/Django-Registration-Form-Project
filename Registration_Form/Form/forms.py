f082c9039331b218d58229f3cef39a56175bbc31
Pheonix <yashpurwar09@gmail.com>
2024-10-26 18:09:52 +0530

    Added CSS using bootstrap cdn Added model in model.py

--- Registration_Form/Form/forms.py
+++ Registration_Form/Form/forms.py
@@ -5,6 +5,23 @@ class Registartion_form(forms.ModelForm):
     class Meta:
         model=registration_model
         fields='__all__'
+        widgets={
+            'first_name':forms.TextInput(attrs={
+              'class':'form-control',  
+            }),
+            'last_name':forms.TextInput(attrs={
+              'class':'form-control',  
+            }),
+            'address':forms.TextInput(attrs={
+              'class':'form-control',  
+            }),
+            'mobile_number':forms.NumberInput(attrs={
+              'class':'form-control',  
+            }),
+            'email':forms.EmailInput(attrs={
+              'class':'form-control',  
+            }),
+        }
     
         
     
--- Registration_Form/Form/models.py
+++ Registration_Form/Form/models.py
@@ -6,3 +6,4 @@ class registration_model(models.Model):
     last_name=models.CharField(max_length=200)
     address=models.CharField(max_length=200)
     mobile_number=models.CharField(max_length=10)
+    email=models.EmailField()
